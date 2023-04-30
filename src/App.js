570b7de321cba9a3f54c44db3dde905b8854fd75
Lakisha J <lakishajohnson@pursuit.org>
2023-04-30 10:23:43 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import star from './blue-star-small.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={star} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
